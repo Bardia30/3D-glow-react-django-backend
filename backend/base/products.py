33798c577058a976169ba3c660b2a1bbec46ac69
Bardia30 <bardiadehbasti@gmail.com>
2023-05-12 01:19:29 -0400

    created Django backend, linked to react frontend, created sqlite database

---
+++ backend/base/products.py
@@ -0,0 +1,81 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'PS2 RGB Light Box',
+    'image': '/images/PS-3-Edited.jpg',
+    'description':
+      'Elevate you Gaming setup with this Ps2 RGB Wall Art Light Box. Perfect Gift for Gamers and Streamers and the best Man Cave decoration piece.',
+    'brand': '3D Glow',
+    'category': 'Gaming',
+    'price': 178.88,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'Lightning Bolt RGB Wall Light',
+    'image': '/images/LB6-Edited.jpg',
+    'description':
+      'The Lightning Bolt RGB sign and Wall Art, electrifies your room like no other Neon Sign could. Best gift for Gamers and RGB gaming light lovers.',
+    'brand': '3D Glow',
+    'category': 'Gaming',
+    'price': 93.78,
+    'countInStock': 7,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'Piano RGB Wall Art',
+    'image': '/images/PI1-edited.jpg',
+    'description':
+      '3D Printed RGB Light Box and Wall Art. Perfect Wall Decor and gift for music and piano lovers. Best RGB lighting solution for your streaming background',
+    'brand': '3D Glow',
+    'category': 'Music',
+    'price': 167.15,
+    'countInStock': 5,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'Deathly Hallows RGB Light Box',
+    'image': '/images/dh3-edited.jpg',
+    'description':
+      'Perfect RGB Lighting Option and gift for Harry Potter Lovers and Wizards. This 3D Printed, RGB Powered Art piece makes your room magical',
+    'brand': '3D Glow',
+    'category': 'Harry Potter',
+    'price': 121.00,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'Dollar Sign RGB Wall Art',
+    'image': '/images/dollar.jpg',
+    'description':
+      'Dollar Sign for hustlers. Way cooler than Neon Signs. Get yours today',
+    'brand': '3D Glow',
+    'category': 'Business',
+    'price': 123.58,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'Electric Guitar RGB Wall Art',
+    'image': '/images/EG1-edited-2.jpg',
+    'description':
+      'Perfect Lighting option for Music and Guitar lovers. Way cooler than Neon Signs. Rock your room decoration today',
+    'brand': '3D Glow',
+    'category': 'Music',
+    'price': 171.05,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+]
+
